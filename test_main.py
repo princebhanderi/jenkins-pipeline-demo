696e0f53dedf18720fefbc2559c18123230ff268
princebhanderi <167297609+princebhanderi@users.noreply.github.com>
2025-10-29 09:44:04 +0530

    6

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import add, subtract
 def test_addition():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(3,3) == 3
+    assert add(3,3) == 6
 
 def test_subtraction():
     assert subtract(5, 2) == 3
